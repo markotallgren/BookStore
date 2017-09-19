86649aeb0b60911131d9df34b2f470d4ec94c9ff
Tallgren Marko <a1600545@myy.haaga-helia.fi>
2017-09-19 10:08:27 +0300

    first BookStore commit

---
+++ src/main/java/com/example/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class BookController {
+
+	@RequestMapping	(value="/index", method=RequestMethod.GET)
+	public String hello(Model model) {	
+		return "index";
+		}
+}
---
+++ src/main/java/com/example/domain/Book.java
@@ -0,0 +1,47 @@
+package com.example.domain;
+
+public class Book {
+	
+	String title;
+	String author;
+	int year;
+	String isbn;
+	double price;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
+				+ "]";
+	}
+}
